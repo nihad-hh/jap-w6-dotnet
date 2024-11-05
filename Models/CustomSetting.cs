50fcd7b9b5e3ca1ea51a8392065a16b04f1843ed
Nihad Hodzic <nihad.hodzic@htecgroup.com>
2024-11-05 13:49:53 +0100

    added manifest file

---
+++ Models/CustomSetting.cs
@@ -0,0 +1,8 @@
+namespace dotnetcoresample.Models
+{
+    public class CustomSettings
+    {
+        public string Var1 { get; set; }
+        public string Var2 { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using dotnetcoresample.Models;
+
+namespace dotnetcoresample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CustomSettings _customSettings;
+
+        public IndexModel(IOptions<CustomSettings> customSettings)
+        {
+            _customSettings = customSettings.Value;
+        }
+
+        public string Var1 { get; private set; }
+        public string Var2 { get; private set; }
+
+        public void OnGet()
+        {
+            Var1 = _customSettings.Var1;
+            Var2 = _customSettings.Var2;
+        }
+    }
+}
